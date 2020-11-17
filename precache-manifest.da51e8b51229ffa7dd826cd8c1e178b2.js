b35238040f8957896fed78ee8608a9a496ffcbf1
Bickey Jaisawal <jvickram2@gmail.com>
2020-11-17 15:24:37 +0530

    Updates

---
+++ precache-manifest.da51e8b51229ffa7dd826cd8c1e178b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fcc8000d8ebe7bf07f20",
+    "url": "/MovieSearh/static/css/main.4f895228.chunk.css"
+  },
+  {
+    "revision": "fcc8000d8ebe7bf07f20",
+    "url": "/MovieSearh/static/js/main.fcc8000d.chunk.js"
+  },
+  {
+    "revision": "77b7197c04d71a0943b0",
+    "url": "/MovieSearh/static/js/1.77b7197c.chunk.js"
+  },
+  {
+    "revision": "986c2919d37397ceab7b",
+    "url": "/MovieSearh/static/js/runtime~main.986c2919.js"
+  },
+  {
+    "revision": "89da4bdebbb5021512089ee2bee8b0b8",
+    "url": "/MovieSearh/index.html"
+  }
+];
